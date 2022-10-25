749acb7d7382197f6df676bd052fad8bd69b6562
ivan-sakota2 <89006537+ivan-sakota2@users.noreply.github.com>
2022-10-25 14:33:47 +0200

    minor css change

--- src/components/NextDay.js
+++ src/components/NextDay.js
@@ -7,25 +7,31 @@ function NextDay(props) {
 
   return (
     <div className="card">
-      <div className="container1">
+      <div className="bar">
         <h3>Time</h3>
-        {path.map((time) => {
-          if (
-            `${year}-${month}-${day}` ===
-            time.time.substring(0, time.time.lastIndexOf("T"))
-          ) {
-            return (
-              <li key={time.time}>
-                {time.time.substring(11, time.time.lastIndexOf(":"))}
-              </li>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <h3>Symbol</h3>
+        <h3>Temp. C°</h3>
+        <h3>Wind m/s</h3>
+        <h3>Precip. mm</h3>
       </div>
-      <div className="container5">
-      <h3>Symbol</h3>
+      <div className="container-wrapper">
+        <div className="container1">
+          {path.map((time) => {
+            if (
+              `${year}-${month}-${day}` ===
+              time.time.substring(0, time.time.lastIndexOf("T"))
+            ) {
+              return (
+                <li key={time.time}>
+                  {time.time.substring(11, time.time.lastIndexOf(":"))}
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+        <div className="container5">
           {path.map((temp) => {
             if (
               `${year}-${month}-${day}` ===
@@ -35,7 +41,7 @@ function NextDay(props) {
                 <li key={temp.time}>
                   {temp.data.next_1_hours ? (
                     <img
-                      style={{ height: "15px", width: "15px" }}
+                      style={{ height: "60px", width: "60px" }}
                       className={"symbol-icon"}
                       src={`./assets/${temp.data.next_1_hours.summary.symbol_code}.png`}
                     ></img>
@@ -47,56 +53,52 @@ function NextDay(props) {
             }
           })}
         </div>
-      <div className="container2">
-        <h3>Temp. C°</h3>
-        {path.map((temp) => {
-          if (
-            `${year}-${month}-${day}` ===
-            temp.time.substring(0, temp.time.lastIndexOf("T"))
-          ) {
-            return (
-              <li key={temp.time}>
-                {Math.round(temp.data.instant.details.air_temperature)} °
-              </li>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </div>
-      <div className="container3">
-        <h3>Wind m/s</h3>
-        {path.map((wind) => {
-          if (
-            `${year}-${month}-${day}` ===
-            wind.time.substring(0, wind.time.lastIndexOf("T"))
-          ) {
-            return (
-              <li key={wind.time}>{wind.data.instant.details.wind_speed}</li>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </div>
-      <div className="container4">
-        <h3>Precip. mm</h3>
-        {path.map((rain) => {
-          if (
-            `${year}-${month}-${day}` ===
-            rain.time.substring(0, rain.time.lastIndexOf("T"))
-          ) {
-            return (
-              <li key={rain.time}>
-                {rain.data.next_1_hours.details.precipitation_amount}
-              </li>
-            );
-          } else {
-            return null;
-          }
-        })}
-
-      
+        <div className="container2">
+          {path.map((temp) => {
+            if (
+              `${year}-${month}-${day}` ===
+              temp.time.substring(0, temp.time.lastIndexOf("T"))
+            ) {
+              return (
+                <li key={temp.time}>
+                  {Math.round(temp.data.instant.details.air_temperature)} °
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+        <div className="container3">
+          {path.map((wind) => {
+            if (
+              `${year}-${month}-${day}` ===
+              wind.time.substring(0, wind.time.lastIndexOf("T"))
+            ) {
+              return (
+                <li key={wind.time}>{wind.data.instant.details.wind_speed}</li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+        <div className="container4">
+          {path.map((rain) => {
+            if (
+              `${year}-${month}-${day}` ===
+              rain.time.substring(0, rain.time.lastIndexOf("T"))
+            ) {
+              return (
+                <li key={rain.time}>
+                  {rain.data.next_1_hours.details.precipitation_amount}
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
       </div>
     </div>
   );
